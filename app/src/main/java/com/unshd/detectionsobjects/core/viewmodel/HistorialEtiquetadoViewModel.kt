6a6048a0a0c938287d1ffed6c6a2ac701fe8a88c
uns <uns@unshd.com>
2025-07-22 18:25:08 -0600

    historialDetecciones 2/2

--- app/src/main/java/com/unshd/detectionsobjects/core/viewmodel/HistorialEtiquetadoViewModel.kt
+++ app/src/main/java/com/unshd/detectionsobjects/core/viewmodel/HistorialEtiquetadoViewModel.kt
@@ -1,7 +1,11 @@
 package com.unshd.detectionsobjects.core.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.unshd.detectionsobjects.core.db.label.ImageLabelDao
+import com.unshd.detectionsobjects.core.db.text.TextBlockDao
+import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
 
-class HistorialEtiquetadoViewModel@Inject constructor(): ViewModel() {
+@HiltViewModel
+class HistorialEtiquetadoViewModel@Inject constructor(private val dao: ImageLabelDao): ViewModel() {
 }
